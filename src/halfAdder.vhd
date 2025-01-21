a68b47e3ac1b637070ae6ced456e5ff359f3dcf9
windowsActual <177792113+windowsActual@users.noreply.github.com>
2025-01-21 14:24:11 -0700

    Finished the project Yay!

--- src/halfAdder.vhd
+++ src/halfAdder.vhd
@@ -29,8 +29,8 @@ entity halfAdder is
   port(
 	i_A     : in  std_logic; -- 1-bit input port
 	i_B     : in  std_logic; 
-	o_S     : out std_logic  -- 1-bit output port (NOTE: NO semicolon on LAST port only!)	    
-	-- TODO:  Carry port
+	o_S     : out std_logic;  -- 1-bit output port (NOTE: NO semicolon on LAST port only!)	    
+	o_Cout  : out std_logic
   ); -- the semicolon is here instead
 end halfAdder;
 
@@ -43,5 +43,6 @@ begin
 	-- *concurrent* signal assignments
 	o_S    <= i_A xor i_B;
 	-- TODO:  Carry signal assignment
+	o_Cout     <= i_A and i_B;
 	
 end halfAdder_arch;
--- src/halfAdder_tb.vhd
+++ src/halfAdder_tb.vhd
@@ -20,21 +20,26 @@ end halfAdder_tb;
 architecture test_bench of halfAdder_tb is 
 	
   -- declare the component of your top-level design unit under test (UUT)
-  -- this must exactly match entity declaration of the thing you are testing
   component halfAdder is
-	port(
-		-- TODO: copy in port map from halfAdder.vhd
-	); -- the semicolon is here instead	
+  port(
+    i_A     : in  std_logic; -- 1-bit input port
+    i_B     : in  std_logic;
+    o_S     : out std_logic;  -- 1-bit output port
+    o_Cout  : out std_logic
+              -- (NOTE: NO semicolon on LAST port only!)
+  ); -- the semicolon is here instead
   end component;
 
   
   -- declare signals needed to stimulate the UUT inputs
   signal w_sw1 : std_logic := '0';
   -- TODO:  sw0 signal
+  signal w_sw0 : std_logic := '0';
   
   -- also need signals for the outputs of the UUT
   signal w_led1 : std_logic := '0';
   -- TODO:  led0 signal
+  signal w_led0 : std_logic := '0';
 
   
 begin
@@ -44,7 +49,8 @@ begin
 	halfAdder_inst : halfAdder port map (
 		i_A     => w_sw1, -- notice comma (not a semicolon)
 		i_B     => w_sw0,
-		o_S     => w_led0 -- no comma on LAST one
+		o_S     => w_led0,
+		o_Cout  => w_led1 -- no comma on LAST one
 		-- TODO:  map Cout 
 	);
 
@@ -56,12 +62,18 @@ begin
 	test_process : process 
 	begin
 	
-		 w_sw1 <= '0'; w_sw0 <= '0'; wait for 10 ns;
-		    -- read as "check that w_lec0 = '0' and if it doesn't then give the message 'bad sum' with an error."
-            assert w_led0 = '0' report "bad sum" severity error;
-            assert w_led1 = '0' report "bad carry" severity error;
-		-- TODO:  rest of test plan
-		
+    w_sw1 <= '0'; w_sw0 <= '0'; wait for 10 ns;
+      assert w_led0 = '0' report "bad sum" severity error;
+      assert w_led1 = '0' report "bad carry" severity error;
+    w_sw1 <= '0'; w_sw0 <= '1'; wait for 10 ns;
+      assert w_led0 = '1' report "bad sum" severity error;
+      assert w_led1 = '0' report "bad carry" severity error;
+    w_sw1 <= '1'; w_sw0 <= '0'; wait for 10 ns;
+      assert w_led0 = '1' report "bad sum" severity error;
+      assert w_led1 = '0' report "bad carry" severity error;
+    w_sw1 <= '1'; w_sw0 <= '1'; wait for 10 ns;
+      assert w_led0 = '0' report "bad sum" severity error;
+      assert w_led1 = '1' report "bad carry" severity error;
 		wait; -- wait forever
 	end process;	
 	-----------------------------------------------------	
